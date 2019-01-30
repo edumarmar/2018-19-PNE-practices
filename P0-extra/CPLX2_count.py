d2eaf04c1c2fe6e2d036207bbcc25398f142cecf
edumart <edumarmar@live.com>
2019-01-30 17:55:59 +0100

    CLASS PRACTICES

---
+++ P0-extra/CPLX2_count.py
@@ -0,0 +1,14 @@
+with open('CPLX2.txt', 'r') as f:
+    a=0
+    c=0
+    t=0
+    g=0
+    for line in f:
+        line=line.lower()
+        if '>' in line:
+            line=''
+        a += line.count('a')
+        c += line.count('c')
+        t += line.count('t')
+        g += line.count('g')
+print('A: ', a, '\nC: ', c, '\nT: ', t,'\nG: ', g)
---
+++ P0-extra/CPLX2_print.py
@@ -0,0 +1,4 @@
+with open('CPLX2.txt', 'r') as f:
+    for line in f:
+        print(line)
+f.close()
