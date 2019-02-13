a255b077ee1b2c17a48aacf9d01a8fbbedc0137c
edumart <edumarmar@live.com>
2019-02-13 18:00:10 +0100

    CLASS PRACTICES

--- P2/clientp2.py
+++ P2/clientp2.py
@@ -1,19 +1,28 @@
 # Programming our fist client
 
 import socket
+from seqp2 import Seq
+
 
 # create a socket for communicating with the server)
 
 
 print('socket created')
 
-PORT= 8080
-IP= '212.128.253.103'
+PORT= 8081
+IP= '212.128.253.102'
+
+
 
 while True:
+    sequence= input('Enter the sequence that you want to analyze: ')
+    s1 = Seq(sequence)
+    compl=(s1.complement())
+    rev=(s1.reverse())
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((IP, PORT))
-    s.send(str.encode(input(' Type your message: ')))
+    s.send(str.encode(compl))
+    s.send(str.encode(rev))
     s.close()
 
 msg= s.recv(2048).decode('utf-8')
