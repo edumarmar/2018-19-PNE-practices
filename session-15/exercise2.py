e2ffbf98f247f68cefe8c116c9e030bfe96a32b2
edumart <edumarmar@live.com>
2019-03-12 17:09:18 +0100

    Session 15 files (error html not implemented yet)

---
+++ session-15/exercise2.py
@@ -0,0 +1,79 @@
+import http.server
+import socketserver
+import termcolor
+
+# Define the Server's port
+PORT = 8008
+
+# Class with our Handler. It is a called derived from BaseHTTPRequestHandler
+# It means that our class inheritates all his methods and properties
+class TestHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+
+        # printing the request line
+        termcolor.cprint(self.requestline, 'green')
+        print('Path: ' + self.path)
+
+        path= self.path
+
+        i=path.find('=')
+
+        f = open("form_exercise2.html", 'r')
+        content = f.read()
+
+        print('Path: ' + self.path)
+
+        path = self.path
+
+        i = path.find('=')
+
+        if i != -1:
+
+            if path.find('&')!=-1:
+                i2=path.find('&')
+                msg = path[i + 1: i2]
+                msg=msg.upper()
+                msg=msg.replace('+', ' ')
+            else:
+                msg=path[i+1:]
+
+            f = open('echohtml.html', 'r')
+            content = f.read()
+            content=content.replace('msg', msg)
+
+
+
+
+        # generating the response message
+        self.send_response(200)
+
+        # defining the header:
+        self.send_header('Content-Type', 'text/html')
+        self.send_header('Content-Length', len(str.encode(content)))
+        self.end_headers()
+
+        # Send the response message
+        self.wfile.write(str.encode(content))
+
+        return
+
+
+
+# - Server MAIN program
+
+with socketserver.TCPServer(("", PORT), TestHandler) as httpd:
+
+    print("Serving at PORT", PORT)
+
+    # -- Main loop: Attend the client. Whenever there is a new
+    # -- clint, the handler is called
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("")
+        print("Stoped by the user")
+        httpd.server_close()
+
+print("")
+print("Server Stopped")
---
+++ session-15/server.py
@@ -0,0 +1,53 @@
+import http.server
+import socketserver
+import termcolor
+
+# Define the Server's port
+PORT = 8002
+
+
+# Class with our Handler. It is a called derived from BaseHTTPRequestHandler
+# It means that our class inheritates all his methods and properties
+class TestHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+
+        # printing the request line
+        termcolor.cprint(self.requestline, 'green')
+
+        # opening and reading the form1.html file
+        f = open("form2.html", 'r')
+        contents = f.read()
+
+        # generating the response message
+        self.send_response(200)
+
+        # defining the header:
+        self.send_header('Content-Type', 'text/html')
+        self.send_header('Content-Length', len(str.encode(contents)))
+        self.end_headers()
+
+        # Send the response message
+        self.wfile.write(str.encode(contents))
+
+        return
+
+
+
+# - Server MAIN program
+
+with socketserver.TCPServer(("", PORT), TestHandler) as httpd:
+
+    print("Serving at PORT", PORT)
+
+    # -- Main loop: Attend the client. Whenever there is a new
+    # -- clint, the handler is called
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("")
+        print("Stoped by the user")
+        httpd.server_close()
+
+print("")
+print("Server Stopped")
